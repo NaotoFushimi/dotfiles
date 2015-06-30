2bc77d6506a775683e38705be76fcbb2809bea61
fushiminaoto <wano_fushimi@icloud.com>
2015-06-30 14:20:39 +0900

    perl

--- setup.pl
+++ setup.pl
@@ -1,7 +1,15 @@
-`cp .vimrc ~/`;
-`cp .zshrc ~/`;
-`source .zshrc`;
+
+`cp vimrc ~/.vimrc`;
+`cp zshrc ~/.zshrc`;
+`source ~/.zshrc`;
 `mkdir -p ~/.vim/colors`;
+`mkdir -p ~/.vim/bundle`;
 `git clone https://github.com/tomasr/molokai`;
 `mv molokai/colors/molokai.vim ~/.vim/colors/`;
 `rm -rf molokai`;
+`cd ~/`;
+`cd ~/.vim/bundle`;
+`git clone git://github.com/Shougo/neobundle.vim.git  ~/.vim/bundle/`;
+`vim +":NeoBundleClean" +:q`;
+`vim +":NeoBundleInstall" +:q`;
+`cd .vim/bundle/neosnippet-snippets`;
