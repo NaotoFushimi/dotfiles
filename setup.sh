2901c691fa7c8fcfb584fb60a6e3a0a599b1eb81
docker_test <fushimi@wano.co.jp>
2015-08-06 01:57:08 +0000

    pl-

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+git pull origin master;
+cp vimrc $HOME/.vimrc;
+cp zshrc $HOME/.zshrc;
+cp -r sheet $HOME/.sheet;
+cp -r vimdoc $HOME/.vimdoc;
+source $HOME/.zshrc;
+mkdir -p $HOME/.vim/colors;
+mkdir -p $HOME/.vim/bundle;
+git clone https://github.com/NaotoFushimi/molokai.git;
+mv molokai/colors/molokai.vim $HOME/.vim/colors/;
+rm -rf molokai;
+cd $HOME/;
+cd $HOME/.vim/bundle;
+git clone git://github.com/Shougo/neobundle.vim.git  $HOME/.vim/bundle/;
+vim +":NeoBundleClean" +:q;
+vim +":NeoBundleInstall" +:q;
+cd .vim/bundle/neosnippet-snippets;
