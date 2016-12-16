50579b684295faf312139de7b37cb8b718492f8b
fushiminaoto <wano_fushimi@icloud.com>
2016-12-16 11:28:26 +0900

    fix gather

--- gather.sh
+++ gather.sh
@@ -1,4 +1,7 @@
 cp ~/.zshrc ./zshrc;
 cp ~/.vimrc ./vimrc;
+
+rm -rf ./sheets;
+rm -rf ./vimdoc;
 cp -r ~/.sheets ./sheets;
 cp -r ~/.vimdoc ./vimdoc;
--- sheets/.sheets/sheets/readme.txt
+++
@@ -1,5 +0,0 @@
-gem install sheet
-export EDITOR=vim
-で使える。
-
-怒られたらchmod -R 755 しまくればOK
